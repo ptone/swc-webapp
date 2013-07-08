6adaae1e3735fa2796c1dc561533bf0b775c78a4
Preston Holmes <preston@ptone.com>
2013-07-08 14:23:30 -0700

    start on basic bootcamp list view

--- project/urls.py
+++ project/urls.py
@@ -2,13 +2,16 @@ from django.conf.urls import patterns, include, url
 from django.views.generic import TemplateView
 
 from django.contrib import admin
+
+from swc.views import UpcomingBootcamps
+
 admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'project.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
+    url(r'^bootcamps/upcoming/', UpcomingBootcamps.as_view(), name='upcoming_bootcamps'),
     url(r'^admin/', include(admin.site.urls)),
     (r'^browserid/', include('django_browserid.urls')),
     url(r'^$', TemplateView.as_view(template_name='home.html'), name='home'),
--- swc/views.py
+++ swc/views.py
@@ -1,1 +1,13 @@
-# Create your views here.
+from django.views.generic.list import ListView
+
+from swc.models import SWCEvent
+
+class EventList(ListView):
+
+    model = SWCEvent
+    template_name = 'bootcamp_list.html'
+
+class UpcomingBootcamps(EventList):
+    def get_queryset(self):
+        self.model.upcoming.all()
+
