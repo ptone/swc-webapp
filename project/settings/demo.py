33c156da6619ef0e338f1337a36b1d257fdafe98
Preston Holmes <preston@ptone.com>
2013-07-09 16:44:01 -0700

    fix check_output call again

--- project/settings/demo.py
+++ project/settings/demo.py
@@ -2,4 +2,6 @@ from .dev import *
 
 from subprocess import check_output
 
-SITE_URL = check_output('curl http://pytohost.com/ip.php', shell=True).strip()
+SITE_URL = "http://{}".format(
+        check_output('curl http://pytohost.com/ip.php', shell=True).strip()
+        )
