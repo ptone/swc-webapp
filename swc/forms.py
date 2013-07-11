341662c950181161b41c9b8de6f759c4acb231a1
Preston Holmes <preston@ptone.com>
2013-07-10 23:10:38 -0700

    check that we have geo data

--- swc/forms.py
+++ swc/forms.py
@@ -17,20 +17,20 @@ class ProfileForm(forms.ModelForm):
                 }
 
     class Media:
-        js = [  'https://maps.googleapis.com/maps/api/js?sensor=false',
+        js = ['https://maps.googleapis.com/maps/api/js?sensor=false',
                 'js/bootstrap/bootstrap-typeahead.js',
                 'js/jquery.addresspicker.js',
                 'js/jquery.json-2.4.min.js',
-                # 'js/profile_edit.js',
              ]
 
     def save(self, force_insert=False, force_update=False, commit=True):
         m = super(ProfileForm, self).save(commit=False)
-        m.geodata = json.loads(self.cleaned_data['new_geo'])
-        # TODO extract fields from this blob
-        m.geo_type = m.geodata['geometry']['location_type']
-        m.lat = m.geodata['lat_val']
-        m.long = m.geodata['long_val']
+        if self.cleaned_data['new_geo']:
+            m.geodata = json.loads(self.cleaned_data['new_geo'])
+            # TODO extract fields from this blob
+            m.geo_type = m.geodata['geometry']['location_type']
+            m.lat = m.geodata['lat_val']
+            m.long = m.geodata['long_val']
         if commit:
             m.save()
         return m
